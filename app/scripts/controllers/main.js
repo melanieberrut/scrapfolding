9ce4bc069437ad50eaf688e1470bb849ee790897
Melanie Berrut <melanieberrut.dev@gmail.com>
2016-07-10 20:14:32 +0100

    APP/DIST all assets working

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scrapfoldingApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the scrapfoldingApp
+ */
+angular.module('scrapfoldingApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
